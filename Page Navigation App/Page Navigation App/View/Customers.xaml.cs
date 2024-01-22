d39905e2f8f5faf2a4723cda41e47c57adadd04d
@huydg1 <huydg@ironhackvietnam.edu.vn>
2024-01-22 12:04:13 +0700

    first commit

---
+++ Page Navigation App/Page Navigation App/View/Customers.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Page_Navigation_App.View
+{
+    /// <summary>
+    /// Interaction logic for Customers.xaml
+    /// </summary>
+    public partial class Customers : UserControl
+    {
+        public double StrokeDashOffsetValue { get; set; }
+        public Customers()
+        {
+            InitializeComponent();
+        }
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            // Bắt đầu Storyboard khi UserControl được nạp
+            Storyboard startAnimation = (Storyboard)FindResource("Start_Animation");
+            if (startAnimation != null)
+            {
+                StrokeDashOffsetValue = 360 * (217.0 / 365.0);
+                startAnimation.Begin(this);
+            }
+        }
+    }
+}
---
+++ Page Navigation App/Page Navigation App/ViewModel/CustomerVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Page_Navigation_App.Model;
+
+namespace Page_Navigation_App.ViewModel
+{
+    class CustomerVM : Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+        public int CustomerID
+        {
+            get { return _pageModel.CustomerCount; }
+            set { _pageModel.CustomerCount = value; OnPropertyChanged(); }
+        }
+
+        public CustomerVM()
+        {
+            _pageModel = new PageModel();
+            DateTime d1 = Convert.ToDateTime("05/15/2023");
+            DateTime d2 = Convert.ToDateTime(DateTime.Now.ToString("MM/dd/yyyy"));
+            TimeSpan time = d2.Subtract(d1);
+            int days = Convert.ToInt32(time.Days);
+            CustomerID = days;
+        }
+    }
+}
